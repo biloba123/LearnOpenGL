8d702714b67d70dd7222f533c2df94c10f6634bd
吕晴阳 <lvqingyang@lvqingyangdemac-mini.local>
2019-04-01 16:16:55 +0800

    Initial Commit

---
+++ TestOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestOpenGL
+//
+//  Created by 吕晴阳 on 2019/4/1.
+//  Copyright © 2019 吕晴阳. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
