2ddd7856dd68d685d8f939ab8706c9da1d1185d6
吕晴阳 <biloba12345@gmail.com>
2019-04-05 21:40:37 +0800

    改变变换顺序

--- TestOpenGL/main.cpp
+++ TestOpenGL/main.cpp
@@ -166,8 +166,8 @@ int main() {
         
         //构造变化矩阵
         glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
         trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
         
         shaderProgram.use();
         glBindVertexArray(vao);
