0c064ddfaf874a906b3a895add72ac8ca5563de4
吕晴阳 <biloba12345@gmail.com>
2019-04-09 11:00:47 +0800

    [mod] Gouraud着色

--- TestOpenGL/resource/colors.fs
+++ TestOpenGL/resource/colors.fs
@@ -1,30 +1,11 @@
 #version 330 core
-in vec3 normal;
-in vec3 fragPos;
+in vec3 GouraudColor;
 
-out vec4 fragColor;
+out vec4 FragColor;
 
 uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform float strength;
 
 void main() {
-    //环境光照
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-    //漫反射光照
-    vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(lightPos - fragPos);
-    float diff = max(0.0, dot(norm, lightDir));
-    vec3 diffuse = diff * lightColor;
-    //镜面光照
-    float specularStrength  = 0.5;
-    vec3 viewDir = normalize(-fragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(0.0, dot(viewDir, reflectDir)), 32);
-    vec3 specular = specularStrength * spec * lightColor;
-    
-    vec3 result = objectColor * (ambient + diffuse + specular);
-    fragColor = vec4(result, 1.0);
+    vec3 result = objectColor * GouraudColor;
+    FragColor = vec4(result, 1.0);
 }
