9b955b21a1979a6336b26c2f2dd68fa8f419dd33
吕晴阳 <biloba12345@gmail.com>
2019-04-09 11:05:31 +0800

    [style] 规范着色器属性命名

--- TestOpenGL/resource/colors.fs
+++ TestOpenGL/resource/colors.fs
@@ -1,8 +1,8 @@
 #version 330 core
-in vec3 normal;
-in vec3 fragPos;
+in vec3 Normal;
+in vec3 FragPos;
 
-out vec4 fragColor;
+out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
@@ -14,17 +14,17 @@ void main() {
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
     //漫反射光照
-    vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(lightPos - fragPos);
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(0.0, dot(norm, lightDir));
     vec3 diffuse = diff * lightColor;
     //镜面光照
     float specularStrength  = 0.5;
-    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(0.0, dot(viewDir, reflectDir)), 32);
     vec3 specular = specularStrength * spec * lightColor;
     
     vec3 result = objectColor * (ambient + diffuse + specular);
-    fragColor = vec4(result, 1.0);
+    FragColor = vec4(result, 1.0);
 }
--- TestOpenGL/resource/colors.vs
+++ TestOpenGL/resource/colors.vs
@@ -2,8 +2,8 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
-out vec3 normal;
-out vec3 fragPos;
+out vec3 Normal;
+out vec3 FragPos;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,6 +11,6 @@ uniform mat4 projection;
 
 void main() {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    normal = mat3(transpose(inverse(model))) * aNormal;
-    fragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(model))) * aNormal;
+    FragPos = vec3(model * vec4(aPos, 1.0));
 }
