cba7c84eb214adf2fc929caa24809bdfba87632d
吕晴阳 <biloba12345@gmail.com>
2019-04-15 16:45:32 +0800

    [feat] 灰度化

--- AdvancedOpenGL/resource/framebuffers_screen.fs
+++ AdvancedOpenGL/resource/framebuffers_screen.fs
@@ -7,5 +7,6 @@ uniform sampler2D texture1;
 
 void main() {
     vec4 texColor = texture(texture1, TexCoords);
-    FragColor = texColor;
+    float average = 0.2126 * texColor.r + 0.7152 * texColor.g + 0.0722 * texColor.b;
+    FragColor = vec4(vec3(average), 1.0);
 }
